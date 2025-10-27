5c5a09f2a891cb394542246f54d1f6fac1635c79
vishnumohan7 <63349875+vishnumohan7@users.noreply.github.com>
2025-10-27 11:06:33 +0530

    env added

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -14,6 +14,7 @@ class AppConstants {
     return token;
   }
   
+  
   static String get shopifyStorefrontAccessToken {
     final token = dotenv.env['SHOPIFY_STOREFRONT_TOKEN'];
     if (token == null || token.isEmpty) {
